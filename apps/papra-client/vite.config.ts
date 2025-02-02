d51f19d5e3eebe6087bed4519af9b9013612050d
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-02-02 22:57:50 +0100

    feat(dev): added proxy in client for default backend redirection (#121)

--- apps/papra-client/vite.config.ts
+++ apps/papra-client/vite.config.ts
@@ -16,6 +16,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1221',
+      },
+    },
   },
   build: {
     target: 'esnext',
